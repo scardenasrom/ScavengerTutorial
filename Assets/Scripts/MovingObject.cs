e0f0e7b1a2b00afeae8a976ca6cbbf07a3478789
scardenasrom <sergio.cardenas.rom@gmail.com>
2018-03-07 20:59:19 +0100

    Kept working through to video 7

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingObject : MonoBehaviour {
+
+    public float moveTime = 0.1f;
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rigidBody;
+    private float inverseMoveTime;
+
+	// Use this for initialization
+	protected virtual void Start () {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1f / moveTime;
+	}
+
+    protected bool Move (int xDir, int yDir, out RaycastHit2D hit)
+    {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(xDir, yDir);
+        // Disabling our own boxCollider so the raycasting doesn't hit ourselves
+        boxCollider.enabled = false;
+        hit = Physics2D.Linecast(start, end, blockingLayer);
+        if (hit.transform == null)
+        {
+            StartCoroutine(SmoothMovement(end));
+            return true;
+        }
+        return false;
+    }
+
+    protected virtual void AttemptMove <T>(int xDir, int yDir) where T : Component
+    {
+        RaycastHit2D hit;
+        bool canMove = Move(xDir, yDir, out hit);
+        if (hit.transform == null)
+        {
+            return;
+        }
+        T hitComponent = hit.transform.GetComponent<T>();
+        if (!canMove && hitComponent != null) {
+            OnCantMove(hitComponent);
+        }
+    }
+
+    protected IEnumerator SmoothMovement(Vector3 end)
+    {
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+        while (sqrRemainingDistance > float.Epsilon)
+        {
+            Vector3 newPosition = Vector3.MoveTowards(rigidBody.position, end, inverseMoveTime * Time.deltaTime);
+            rigidBody.MovePosition(newPosition);
+            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+            yield return null;
+        }
+    }
+
+    protected abstract void OnCantMove<T>(T component) where T : Component;
+
+}
